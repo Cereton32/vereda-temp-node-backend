3d8948ae9e16e81b5f58148d7f73f3b584b2d4e5
Abhishek Kumar <2021pcecsabhishek010@poornima.org>
2025-05-20 15:13:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ apiKey.apiKey = process.env.BREVO_API_KEY; // from .env
 
 const transactionalEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
-Send OTP
+
 app.post("/send-otp", async (req, res) => {
   const { email } = req.body;
   if (!email) return res.status(400).json({ error: "Email is required" });
